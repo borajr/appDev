7fb8437fb3c094ade48de42d0ccec4eb9aaa6a05
marko <m.a.mrvelj@student.tue.nl>
2024-03-12 23:19:47 +0100

    first implementation of signup

--- MyApplication/app/src/main/java/com/example/myapplication/LoginActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -3,12 +3,18 @@ package com.example.myapplication;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
 
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Set the content view to your layout for LoginActivity
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+
+
+
     }
 }
