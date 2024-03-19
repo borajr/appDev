dbbccef5d1f30c1f84e3690107c82248a0dc13f6
teomankerki <59364911+teomankerki@users.noreply.github.com>
2024-03-20 00:48:11 +0100

    transitions fixed

--- MyApplication/app/src/main/java/com/example/myapplication/ProfileCreationDetailThree.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ProfileCreationDetailThree.java
@@ -1,4 +1,26 @@
 package com.example.myapplication;
 
-public class ProfileCreationDetailThree {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileCreationDetailThree extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profiledetailthree);
+
+        // Find the button by its ID
+        findViewById(R.id.confirm_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start ProfileCreationDetailThree
+                Intent intent = new Intent(ProfileCreationDetailThree.this, CameraActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
