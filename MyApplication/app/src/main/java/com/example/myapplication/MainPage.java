17574a6fc37811462cc4af629b0bbc42945a88ab
efrrzn <e.razon@student.tue.nl>
2024-03-21 01:44:17 +0100

    manifests

--- MyApplication/app/src/main/java/com/example/myapplication/MainPage.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainPage.java
@@ -6,23 +6,23 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class PreferenceCloneOne extends AppCompatActivity {
+public class MainPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_preference_detail);
+        setContentView(R.layout.activity_main);
 
         // Find the button by its ID
-        View btnConfirm = findViewById(R.id.confirm_button);
+        View btnContinue = findViewById(R.id.btnToChange);
 
 
         // Set OnClickListener to the button
-        btnConfirm.setOnClickListener(new View.OnClickListener() {
+        btnContinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Create an Intent to navigate to the activity_change_password.xml
-                Intent intent = new Intent(PreferenceCloneOne.this, MainPage.class);
+                Intent intent = new Intent(MainPage.this, ChangePassword.class);
                 startActivity(intent); // Start the new activity
             }
         });
--- MyApplication/app/src/main/java/com/example/myapplication/PreferenceCloneTwo.java
+++ MyApplication/app/src/main/java/com/example/myapplication/PreferenceCloneTwo.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,6 @@ public class PreferenceCloneTwo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preference_detailtwo);
+
     }
 }
