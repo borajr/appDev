64f4fdaf707d11d3e411aeff00904a5ff2c645e7
teomankerki <59364911+teomankerki@users.noreply.github.com>
2024-03-21 23:27:47 +0100

    merge correction

--- MyApplication/app/src/main/java/com/example/myapplication/LoginActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -7,9 +7,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     private EditText emailEditText;
     private EditText passwordEditText;
 
--- MyApplication/app/src/main/java/com/example/myapplication/SignupActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/SignupActivity.java
@@ -7,7 +7,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
