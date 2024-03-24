a03774e0db9e8dfbdc88676a0119f5e6764abc4a
boranb01 <132908338+boranb01@users.noreply.github.com>
2024-03-25 00:57:56 +0100

    chat send button.

--- MyApplication/app/src/main/java/com/example/myapplication/SimpleChat.java
+++ MyApplication/app/src/main/java/com/example/myapplication/SimpleChat.java
@@ -1,10 +1,26 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.myapplication.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class SimpleChat extends AppCompatActivity {
 
@@ -12,5 +28,16 @@ public class SimpleChat extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simplechat);
+        Button sendButton = findViewById(R.id.chat_send_button);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        messagesRef = firebaseDatabase.getReference("messages");
+
+    sendButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            boolean correctInput = true;
+        }
     }
-}
+    }
