5719b28613028d47fc0bc11b9d051a66fda38c63
Bora <mbkaplan7@gmail.com>
2024-03-26 18:22:02 +0100

    MainPage

--- MyApplication/app/src/main/java/com/example/myapplication/MainPage.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainPage.java
@@ -3,19 +3,48 @@ package com.example.myapplication;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class MainPage extends AppCompatActivity {
+    private FirebaseFirestore db;
+    private cards cards_data[];
+    private arrayAdapter arrayAdapter;
+    private int i;
+    private FirebaseAuth mAuth;
+
+    private String currentUId;
+    private DatabaseReference usersDb;
+    ListView listView;
+    List<cards> rowItems;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +54,7 @@ public class MainPage extends AppCompatActivity {
 
         // Initialize the BottomNavigationView
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
-
+        checkUserPreferences();
         // Set the OnNavigationItemSelectedListener
         bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -54,7 +83,134 @@ public class MainPage extends AppCompatActivity {
             showMatchPopup();
         }
 
+        usersDb = FirebaseDatabase.getInstance().getReference().child("users");
+        currentUId = mAuth.getCurrentUser().getUid();
+
+
+        rowItems = new ArrayList<cards>();
+
+        arrayAdapter = new arrayAdapter(this, R.layout.item, rowItems);
+
+
+        SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView) findViewById(R.id.frame);
+        flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
+            @Override
+            public void removeFirstObjectInAdapter() {
+                // this is the simplest way to delete an object from the Adapter (/AdapterView)
+                Log.d("LIST", "removed object!");
+                rowItems.remove(0);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onLeftCardExit(Object dataObject) {
+                //Do something on the left!
+                //You also have access to the original object.
+                //If you want to use it just cast it (String) dataObject
+                Toast.makeText(MainPage.this, "left", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onRightCardExit(Object dataObject) {
+                Toast.makeText(MainPage.this, "right", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdapterAboutToEmpty(int itemsInAdapter) {
+                // Ask for more data here
+
+            }
+
+            @Override
+            public void onScroll(float scrollProgressPercent) {
+
+            }
+        });
+
+
+        // Optionally add an OnItemClickListener
+        flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClicked(int itemPosition, Object dataObject) {
+                Toast.makeText(MainPage.this, "click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
+    private String userGender;
+    private String nextUserGender;
+
+    public void checkGender() {
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+
+    }
+
+
+    private String userSex;
+    private String userPreferredGender;
+
+    public void checkUserPreferences() {
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference userDb = usersDb.child(user.getUid());
+        userDb.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    if (dataSnapshot.child("gender").getValue() != null && dataSnapshot.child("preferences").child("gender").getValue() != null) {
+                        userSex = dataSnapshot.child("gender").getValue().toString();
+                        userPreferredGender = dataSnapshot.child("preferences").child("gender").getValue().toString();
+                        getOppositeSexUsers();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void getOppositeSexUsers() {
+        usersDb.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                if (!dataSnapshot.getKey().equals(currentUId) && dataSnapshot.child("gender").getValue() != null && dataSnapshot.child("preferences").child("gender").getValue() != null) {
+                    String user2Gender = dataSnapshot.child("gender").getValue().toString();
+                    String user2PreferredGender = dataSnapshot.child("preferences").child("gender").getValue().toString();
+
+                    if (userSex.equals(user2PreferredGender) && user2Gender.equals(userPreferredGender)) {
+                        //display users
+                        String profileImageUrl = "default";
+                        if (!dataSnapshot.child("images").getValue().equals("default")) {
+                            profileImageUrl = dataSnapshot.child("profileImageUrl").getValue().toString();
+                        }
+                        cards item = new cards(dataSnapshot.getKey(), dataSnapshot.child("name").getValue().toString(), profileImageUrl);
+                        rowItems.add(item);
+                        arrayAdapter.notifyDataSetChanged();
+                    }
+                }
+            }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+
 
     private void showMatchPopup() {
         // Inflate the match_popup.xml layout
@@ -68,26 +224,26 @@ public class MainPage extends AppCompatActivity {
 
         // Set random values for location and time from arrays (assuming these arrays exist)
         String[] locations = {"Luna Ground Floor",
-        	    "Hubble",
-        	    "Neuron Cafe",
-        	    "Neuron Upstairs Common Area",
-        	    "Traverse Entrance",
-        	    "Atlas Food City",
-        	    "Metaforum Food City",
-        	    "Gemini Food City",
+                "Hubble",
+                "Neuron Cafe",
+                "Neuron Upstairs Common Area",
+                "Traverse Entrance",
+                "Atlas Food City",
+                "Metaforum Food City",
+                "Gemini Food City",
                 "Flux Food City",
                 "Metaforum Ground Floor",
-        	    "Auditorium In Front of Subway"};
+                "Auditorium In Front of Subway"};
         String[] times = {"Lunch Time Wednesday",
-                	    "Lunch Time Monday",
-                	    "Lunch Time Tuesday",
-                	    "Lunch Time Thursday",
-                	    "Lunch Time Friday",
-                	    "18.00 Wednesday",
-                	    "18.00 Monday",
-                        "18.00 Tuesday",
-                        "18.00 Thursday",
-                        "18.00 Friday" };
+                "Lunch Time Monday",
+                "Lunch Time Tuesday",
+                "Lunch Time Thursday",
+                "Lunch Time Friday",
+                "18.00 Wednesday",
+                "18.00 Monday",
+                "18.00 Tuesday",
+                "18.00 Thursday",
+                "18.00 Friday" };
         Random random = new Random();
         String randomLocation = locations[random.nextInt(locations.length)];
         String randomTime = times[random.nextInt(times.length)];
@@ -100,5 +256,5 @@ public class MainPage extends AppCompatActivity {
         timeTextView.setText("Time: " + randomTime);
 
     }
-}
 
+}
