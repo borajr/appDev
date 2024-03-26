bde52c5e34119655dd3d24b1461bbe34f1678bcd
Bora <mbkaplan7@gmail.com>
2024-03-26 15:41:53 +0100

    mainPage

--- MyApplication/app/src/main/java/com/example/myapplication/MainPage.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainPage.java
@@ -2,44 +2,232 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainPage extends AppCompatActivity {
+    private FirebaseFirestore db;
+    private cards cards_data[];
+    private arrayAdapter arrayAdapter;
+    private int i;
+    private FirebaseAuth mAuth;
+
+    private String currentUId;
+    private DatabaseReference usersDb;
+    ListView listView;
+    List<cards> rowItems;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        // Initialize the BottomNavigationView
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        db = FirebaseFirestore.getInstance();
+        checkUserGender();
 
-        // Set the OnNavigationItemSelectedListener
-        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        //usersDb = FirebaseDatabase.getInstance().getReference().child("users");
+        //mAuth = FirebaseAuth.getInstance();
+        currentUId = mAuth.getCurrentUser().getUid();
+
+        //checkUserSex();
+        checkGender();
+        rowItems = new ArrayList<cards>();
+
+        arrayAdapter = new arrayAdapter(this, R.layout.item, rowItems );
+
+
+        SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView) findViewById(R.id.frame);
+        flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                // Get the item ID
-                int id = item.getItemId();
+            public void removeFirstObjectInAdapter() {
+                // this is the simplest way to delete an object from the Adapter (/AdapterView)
+                Log.d("LIST", "removed object!");
+                rowItems.remove(0);
+                arrayAdapter.notifyDataSetChanged();
+            }
 
-                // Use if-else blocks to determine which item was selected
-                if (id == R.id.navigation_profile) {
-                    startActivity(new Intent(MainPage.this, ProfileSetup.class));
-                    return true;
-                } else if (id == R.id.navigation_main) {
-                    // You can update the UI to indicate this is the current page
-                    // or perform other actions appropriate for clicking "Main"
-                    return true;
-                } else if (id == R.id.navigation_chats) {
-                    startActivity(new Intent(MainPage.this, ChatActivity.class));
-                    return true;
-                }
+            @Override
+            public void onLeftCardExit(Object dataObject) {
+                //Do something on the left!
+                //You also have access to the original object.
+                //If you want to use it just cast it (String) dataObject
+                Toast.makeText(MainPage.this, "left", Toast.LENGTH_SHORT).show();
+            }
 
-                return false;
+            @Override
+            public void onRightCardExit(Object dataObject) {
+                Toast.makeText(MainPage.this, "right", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdapterAboutToEmpty(int itemsInAdapter) {
+                // Ask for more data here
+
+            }
+
+            @Override
+            public void onScroll(float scrollProgressPercent) {
+
+            }
+        });
+
+
+        // Optionally add an OnItemClickListener
+        flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClicked(int itemPosition, Object dataObject) {
+                Toast.makeText(MainPage.this, "click", Toast.LENGTH_SHORT).show();
             }
         });
 
     }
+
+    private String userGender;
+    private String nextUserGender;
+    public void checkGender() {
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+
+    }
+
+
+    public void checkUserGender() {
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference userDb = usersDb.child(user.getUid());
+        userDb.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    String userGender = dataSnapshot.child("gender").getValue(String.class);
+                    String preferredGender = dataSnapshot.child("preferences/gender").getValue(String.class);
+
+                    if (userGender != null && preferredGender != null) {
+
+                        usersDb.orderByChild("gender").equalTo(preferredGender).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot snapshot) {
+                                for (DataSnapshot matchSnapshot : snapshot.getChildren()) {
+                                    String matchId = matchSnapshot.getKey();
+                                    String matchName = matchSnapshot.child("name").getValue(String.class);
+                                    String matchGender = matchSnapshot.child("gender").getValue(String.class);
+                                    String matchPreferredGender = matchSnapshot.child("preferences/gender").getValue(String.class);
+
+                                    // Check if this match has the opposite gender preference
+                                    if (matchGender != null && matchPreferredGender != null) {
+                                        if (userGender.equals(matchPreferredGender) && matchGender.equals(preferredGender)) {
+                                            // If condition holds true, add to the array adapter
+                                            String profileImageUrl = matchSnapshot.child("profileImageUrl").getValue(String.class);
+                                            cards item = new cards(matchId, matchName, profileImageUrl);
+                                            rowItems.add(item);
+                                            arrayAdapter.notifyDataSetChanged();
+                                        }
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError error) {
+
+                            }
+                        });
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+
+    /*private String userSex;
+    private String oppositeUserSex;
+    public void checkUserSex(){
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference userDb = usersDb.child(user.getUid());
+        userDb.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    if (dataSnapshot.child("gender").getValue() != null){
+                        userSex = dataSnapshot.child("gender").getValue().toString();
+                        switch (userSex){
+                            case "Male":
+                                oppositeUserSex = "Female";
+                                break;
+                            case "Female":
+                                oppositeUserSex = "Male";
+                                break;
+                        }
+                        getOppositeSexUsers();
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void getOppositeSexUsers(){
+        usersDb.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                if (dataSnapshot.child("gender").getValue() != null) {
+                    if (dataSnapshot.exists() && !dataSnapshot.child("connections").child("nope").hasChild(currentUId) && !dataSnapshot.child("connections").child("right").hasChild(currentUId) && dataSnapshot.child("gender").getValue().toString().equals(oppositeUserSex)) {
+                        String profileImageUrl = "default";
+                        if (!dataSnapshot.child("profileImageUrl").getValue().equals("default")) {
+                            profileImageUrl = dataSnapshot.child("profileImageUrl").getValue().toString();
+                        }
+                        cards item = new cards(dataSnapshot.getKey(), dataSnapshot.child("name").getValue().toString(), profileImageUrl);
+                        rowItems.add(item);
+                        arrayAdapter.notifyDataSetChanged();
+                    }
+                }
+            }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+            }
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }*/
+
+
 }
