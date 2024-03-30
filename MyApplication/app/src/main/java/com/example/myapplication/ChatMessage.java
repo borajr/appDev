669a53b01e2657eef20c707ba7d63cadb7890e9d
boranb01 <132908338+boranb01@users.noreply.github.com>
2024-03-30 19:00:56 +0100

    chat send button.

--- MyApplication/app/src/main/java/com/example/myapplication/ChatMessage.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ChatMessage.java
@@ -1,22 +1,22 @@
 package com.example.myapplication;
 
-import java.util.Date;
-
 public class ChatMessage {
     private String messageText;
 
     public boolean isSent;
     private String messageUser;
-    private long messageTime;
-    public ChatMessage(String messageText, String messageUser) {
+    private String messageTime;
+    public ChatMessage(String messageText, String messageUser){
         this.messageText = messageText;
         this.messageUser = messageUser;
 
         // Initialize to current time
-        messageTime = new Date().getTime();
     }
     public ChatMessage(){
     }
+
+
+
     public String getMessageText() {
         return messageText;
     }
@@ -29,13 +29,6 @@ public class ChatMessage {
     public void setMessageUser(String messageUser) {
         this.messageUser = messageUser;
     }
-    public long getMessageTime() {
-        return messageTime;
-    }
-    public void setMessageTime(long messageTime) {
-        this.messageTime = messageTime;
-    }
-
 
     public boolean isSent() {
         return true;
